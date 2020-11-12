9ecdd7f4a82affaa3dd11313f136c73d16c993c2
jerome <jerome.poirier@yahoo.fr>
2020-11-12 07:51:11 +0100

    Clean code

--- src/items/schemas/item.schema.ts
+++ src/items/schemas/item.schema.ts
@@ -1,22 +1,3 @@
-// import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-// import { Document } from 'mongoose';
-
-// export type ItemDocument = Item & Document;
-
-// @Schema()
-// export class Item {
-//     @Prop()
-//     name: String;
-
-//     @Prop()
-//     qty: Number;
-
-//     @Prop()
-//     description: String;
-// }
-
-// export const ItemSchema = SchemaFactory.createForClass(Item);
-
 import * as mongoose from 'mongoose';
 
 export const ItemSchema = new mongoose.Schema({
